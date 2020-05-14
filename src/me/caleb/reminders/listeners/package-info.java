b98e560ab2e0e646f8d8cd6b602f8c50a8649641
Caleb <caleb.ja.owens@gmail.com>
2020-05-13 23:29:56 -0400

    Starting Reminders 3.0.0. Doing a complete re-write

--- src/me/caleb/reminders/listeners/package-info.java
+++
@@ -1,1 +0,0 @@
-package me.caleb.reminders.listeners;
--- src/me/caleb/reminders/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package me.caleb.reminders.utils;
-
-import org.bukkit.ChatColor;
-
-public class Utils {
-	public static String chat(String s) {
-		return ChatColor.translateAlternateColorCodes('&', s);
-	}
-}
